eef09b9eeb24d2a2d2c6af0d0ad4940f71c79fca
shenyu <shenyu@shenyu.me>
2018-04-20 12:32:19 +0800

    Update.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ std::wstring StringToWstring(const std::string& src_str) {
     wchar_t* dst_wstr = new wchar_t[max_len];
 #ifdef _WIN32
     size_t out_size;
-    mbstowcs_s(&out_size, dst_wstr, src_str.c_str(), max_len);
+    mbstowcs_s(&out_size, dst_wstr, max_len, src_str.c_str(), src_str.length());
 #else
     mbstowcs(dst_wstr, src_str.c_str(), max_len);
 #endif
@@ -24,7 +24,7 @@ std::string WstringToString(const std::wstring & src_wstr) {
     char* dst_str = new char[max_len];
 #ifdef _WIN32
     size_t out_size;
-    wcstombs_s(&out_size, dst_str, src_wstr.c_str(),max_len);
+    wcstombs_s(&out_size, dst_str, max_len, src_wstr.c_str(), src_wstr.length());
 #else
     wcstombs(dst_str, src_wstr.c_str(),max_len);
 #endif
